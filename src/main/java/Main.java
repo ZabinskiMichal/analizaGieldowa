0aeca8211022d59922cebab0d2b19df9e35f228b
Michal Zabinski <zabinski@student.agh.edu.pl>
2022-06-26 15:09:42 +0200

    wydzieloneDoKlas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,72 +1,14 @@
-//import javax.mail.MessagingException;
-//import javax.mail.Transport;
-//import javax.mail.internet.MimeMessage;
-//import javax.sql.rowset.serial.SerialStruct;
-//
-//
-////to utworzenia mail sendera wykorzystamy protokoł SMTP, czyli standart mail sender protocol
-//
-//
-////bedziemy musieli skonfigurowac
-////1) polacznie z protokolem SNTP
-////2)  autorzacje z poczta email
-//
-//
-//public class Main {
-//
-//    public static void main (String[] args){
-//
-//        try{
-//            MimeMessage message = MailMessagePrepare.prepareTextMessageObject("michal@frognet.com.pl", "tutlEmaila", "elo mordeczko");
-//            Transport.send(message);
-//
-//        }catch (MessagingException e){
-//            throw new RuntimeException(e.getMessage());
-//
-//        }
-//
-//    }
-//}
 
-import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import java.util.Properties;
+
 
 public class Main {
     public static void main(String[] args) {
 
-        String host="poczta.o2.pl";
-        final String user="zabinskimichal@o2.pl";//change accordingly
-        final String password="Zabka123";//change accordingly
+        mailConfig mail1 = new mailConfig("poczta.o2.pl", "zabinskimichal@o2.pl", "Zabka123", "michal@frognet.com.pl");
 
-        String to="michal@frognet.com.pl";//change accordingly
+        if(true){
+            mail1.sendMail(mail1.createConnection(), "temat", "dzis mamy spadki na rynku");
+        }
 
-        //Get the session object
-        Properties props = new Properties();
-        props.put("mail.smtp.host",host);
-        props.put("mail.smtp.auth", "true");
-
-        Session session = Session.getDefaultInstance(props,
-                new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(user,password);
-                    }
-                });
-
-        //Compose the message
-        try {
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(user));
-            message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));
-            message.setSubject("javatpoint");
-            message.setText("This is simple program of sending email using JavaMail API");
-
-            //send the message
-            Transport.send(message);
-
-            System.out.println("message sent successfully...");
-
-        } catch (MessagingException e) {e.printStackTrace();}
     }
 }
